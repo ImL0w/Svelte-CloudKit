e78bc12a9c3743ffb14433e76cd137ecba06b372
Maximilian Trimarchi <maxipaltri@gmail.com>
2022-11-04 15:21:22 +0100

    root components export

---
+++ CloudKit/src/lib/index.ts
@@ -0,0 +1,9 @@
+import Button from './components/user/button.svelte';
+import CardContainer from './components/layout/CardContainer.svelte';
+import Checkbox from './components/user/checkbox.svelte';
+import ContextMenu from './components/user/context-menu/contextMenu.svelte';
+import Datatable from './components/user/data-table/horizontalTable.svelte';
+import Dropdown from './components/user/dropdown.svelte';
+import Tag from './components/layout/Tag.svelte';
+
+export { Button, CardContainer, Checkbox, ContextMenu, Datatable, Dropdown, Tag };
