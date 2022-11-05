4cbc540658371423396a92f7a4c254b86ca4d77d
Maximilian Trimarchi <maxipaltri@gmail.com>
2022-11-05 14:35:32 +0100

    added inputs to export

--- CloudKit/src/lib/index.ts
+++ CloudKit/src/lib/index.ts
@@ -4,6 +4,18 @@ import Checkbox from './components/user/checkbox.svelte';
 import ContextMenu from './components/user/context-menu/contextMenu.svelte';
 import Datatable from './components/user/data-table/horizontalTable.svelte';
 import Dropdown from './components/user/dropdown.svelte';
+import InputPassword from './components/user/input/inputPassword.svelte';
+import InputText from './components/user/input/inputText.svelte';
 import Tag from './components/layout/Tag.svelte';
 
-export { Button, CardContainer, Checkbox, ContextMenu, Datatable, Dropdown, Tag };
+export {
+	Button,
+	CardContainer,
+	Checkbox,
+	ContextMenu,
+	Datatable,
+	Dropdown,
+	InputPassword,
+	InputText,
+	Tag
+};
