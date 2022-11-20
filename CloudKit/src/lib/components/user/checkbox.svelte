f696bca6d7ac497f526fef8a1ab506be5d8d7924
Im0Low <maxipaltri@gmail.com>
2022-11-20 12:25:08 +0100

    checbox tooltip added

--- CloudKit/src/lib/components/user/checkbox.svelte
+++ CloudKit/src/lib/components/user/checkbox.svelte
@@ -6,20 +6,31 @@
 	export let checked = false;
 	export let label = '';
 	export let size: CloudKit.Theme.FontSizes = 'sm';
+	export let tooltip = '';
 </script>
 
 <h4
-	class={concatClasses(
-		'input-checkbox flex flex-vr gap-2 h-fit w-fit',
-		size !== 'sm' ? `fs-${size}` : '',
-		classList
-	)}
+	class={concatClasses('input-checkbox flex flex-vr gap-2 w-fit', `fs-${size}`, classList)}
+	title={tooltip}
 >
 	<input type="checkbox" bind:checked />
 	{label}
 </h4>
 
 <style lang="scss">
-	.input-checkbox {
+	h4.input-checkbox {
+		&[title]:not([title='']) {
+			position: relative;
+			margin-right: 10.5px;
+
+			&::after {
+				content: '?';
+				color: var(--cyan);
+				font-size: 73%;
+				position: absolute;
+				right: -10px;
+				top: -2.5px;
+			}
+		}
 	}
 </style>
