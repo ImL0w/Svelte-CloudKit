7a54d6ec0e497badba06a2a952110697ba5a3a78
Im0Low <maxipaltri@gmail.com>
2022-11-20 12:25:16 +0100

    radio button component

---
+++ CloudKit/src/lib/components/user/radioButton.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	// UTILITY
+	import { concatClasses } from '$lib/ts/utility/array';
+	// EXPORTS
+	export let checked = false;
+	export let id = '',
+		name = '';
+	export let label = '';
+	export let size: CloudKit.Theme.FontSizes = 'sm';
+	export let tooltip = '';
+</script>
+
+<h4
+	class={concatClasses('input-radio flex flex-vr gap-2 w-fit', `fs-${size}`)}
+	title={tooltip}
+>
+	<input type="radio" {name} {id} {checked} />
+	{label}
+</h4>
+
+<style lang="scss">
+	h4[title]:not([title='']) {
+        margin-right: 10px;
+		position: relative;
+
+		&::after {
+			content: '?';
+			position: absolute;
+			right: -10px;
+			top: -2.5px;
+			color: var(--cyan);
+			font-size: 73%;
+		}
+	}
+</style>
