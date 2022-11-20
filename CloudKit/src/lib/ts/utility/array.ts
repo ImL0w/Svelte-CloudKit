99a1d1fbdf68a2002323fbb053a3b0ed9d51beb3
Im0Low <maxipaltri@gmail.com>
2022-11-20 09:57:47 +0100

    refactor & concatenation function fix

--- CloudKit/src/lib/ts/utility/array.ts
+++ CloudKit/src/lib/ts/utility/array.ts
@@ -1,30 +1,28 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 /**
- * 
  * @param key Object key to store it's value
  * @param objects Array of objects
  * @returns  Normal array
  * @example Input ==> "tony", [{tony: "123"}, {tony: "nana", notony: "no"}]
  * Output ==> ["123", "nana"]
  */
-export function objectsToRaw(key: string, ...objects: object[]): Array<any> {
-	const rawArray = [];
-	objects.forEach((value) => {
-		rawArray.push(value[key]);
-	});
-	return rawArray;
+
+export function extractObjValues(key: string, ...objects: object[]): unknown[] {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	return objects.map((obj) => obj[key] ?? undefined)?.filter((obj) => obj) || [];
 }
 
 /**
- * 
+ *
  * @param classList array of strings to be concatinated with spaces
  * @returns String with desired classes concatinated
  * @example Input ==> ["class1", "class2", "class3"]
  * Output ==> "class1 class2 class3"
  */
 export function concatClasses(...classList: string[]): string {
-	classList.forEach((className, i) => {
-		classList[i] = className.concat(i !== classList.length - 1 ? ' ' : '');
-	});
-	return ''.concat(...classList);
+	return ''.concat(
+		...classList
+			.filter((value) => value)
+			.map((className, index, filtered) => className.concat(index < filtered.length - 1 ? ' ' : ''))
+	);
 }
