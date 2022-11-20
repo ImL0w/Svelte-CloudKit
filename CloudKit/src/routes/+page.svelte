07237701c957e3f1ca5cb7e9c3796bf78a6ee221
Im0Low <maxipaltri@gmail.com>
2022-11-20 12:25:49 +0100

    testing sample

--- CloudKit/src/routes/+page.svelte
+++ CloudKit/src/routes/+page.svelte
@@ -4,23 +4,29 @@
 	import Dropdown from '$lib/components/user/dropdown.svelte';
 	import Tag from '$lib/components/layout/Tag.svelte';
 	import InputArea from '$lib/components/user/input/inputArea.svelte';
+	import RadioButton from '$lib/components/user/radioButton.svelte';
+	import Checkbox from '$lib/components/user/checkbox.svelte';
+	import InputText from '$lib/components/user/input/inputText.svelte';
 
 	const contextMenuConfig: CloudKit.ContextMenu.config = {
 		items: [{ value: 'rename' }, { value: 'alert', func: () => alert(1) }, { value: 'close' }]
 	};
 </script>
 
-<Button theme="primary">Lol</Button>
-
-<ContextMenu items={contextMenuConfig.items} targetsSelector="#app-content h1" />
-
-<Tag color="primary">asdas</Tag>
-
 <div id="index" class="transition-all-250">
-	<div id="app-content" class="flex-col-md">
-		<Dropdown options={[{ img: '', value: 'lol' }, 'lol']} />
+	<div id="app-content">
 		<h1>Welcome!</h1>
-		<InputArea value="Shiiiish" resizable />
+		<div class="flex-col gap-3">
+			<Button>Lol</Button>
+			<Tag color="red">asdas</Tag>
+			<Dropdown options={[{ img: '', value: 'lol' }, 'lol']} />
+			<InputArea value="Shiiiish" resizable />
+			<RadioButton checked label="strunz" name="chk" size="lg" />
+			<RadioButton label="strunz" tooltip="okeeeee" name="chk" />
+			<Checkbox checked label="okay lets go" tooltip="lool" size="lg" />
+			<Checkbox label="okay lets go" />
+			<InputText />
+		</div>
 	</div>
 </div>
 
