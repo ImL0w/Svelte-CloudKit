a97224cc2a6df6136d7a85802e2e1566703c35c8
Im0Low <maxipaltri@gmail.com>
2022-11-20 09:56:23 +0100

    textarea added

---
+++ CloudKit/src/lib/components/user/input/inputArea.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	// UTILITY
+	import { concatClasses } from '$lib/ts/utility/array';
+	// EXPORTS
+	export let classList = '';
+	export let cols = 30,
+		rows = 10;
+	export let hidden = false;
+	export let id = '';
+	export let maxWidth = '',
+		maxHeight = '';
+	export let resizable = false;
+	export let size: CloudKit.Theme.FontSizes = 'xs';
+	export let theme: CloudKit.Theme.Colors = 'dark';
+	export let value = '';
+</script>
+
+<textarea
+	class={concatClasses(theme, `fs-${size}`, classList)}
+	class:hide={hidden}
+	style:max-width={maxWidth}
+	style:max-height={maxHeight}
+	style:resize={!resizable ? 'none' : ''}
+	{id}
+	{cols}
+	{rows}
+	{value}
+/>
--- CloudKit/src/routes/+page.svelte
+++ CloudKit/src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import ContextMenu from '$lib/components/user/context-menu/contextMenu.svelte';
 	import Dropdown from '$lib/components/user/dropdown.svelte';
 	import Tag from '$lib/components/layout/Tag.svelte';
+	import InputArea from '$lib/components/user/input/inputArea.svelte';
 
 	const contextMenuConfig: CloudKit.ContextMenu.config = {
 		items: [{ value: 'rename' }, { value: 'alert', func: () => alert(1) }, { value: 'close' }]
@@ -19,9 +20,12 @@
 	<div id="app-content" class="flex-col-md">
 		<Dropdown options={[{ img: '', value: 'lol' }, 'lol']} />
 		<h1>Welcome!</h1>
+		<InputArea value="Shiiiish" resizable />
 	</div>
 </div>
 
 <style lang="scss" global>
-	@use '../lib/scss/base.scss' with ($boxed: 100px);
+	@use '../lib/scss/base.scss' with (
+		$boxed: 100px
+	);
 </style>
