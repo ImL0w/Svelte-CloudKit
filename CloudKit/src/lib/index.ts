443ead5578594c8ef2dc61788bba3cf293eb06d2
Im0Low <maxipaltri@gmail.com>
2022-11-21 18:48:51 +0100

    progressbar export

--- CloudKit/src/lib/index.ts
+++ CloudKit/src/lib/index.ts
@@ -7,6 +7,7 @@ import Dropdown from './components/user/dropdown.svelte';
 import InputArea from './components/user/input/inputArea.svelte';
 import InputPassword from './components/user/input/inputPassword.svelte';
 import InputText from './components/user/input/inputText.svelte';
+import ProgressBar from './components/layout/progressBar.svelte';
 import RadioButton from './components/user/radioButton.svelte';
 import Tag from './components/layout/Tag.svelte';
 
@@ -20,6 +21,7 @@ export {
 	InputArea,
 	InputPassword,
 	InputText,
+	ProgressBar,
 	RadioButton,
 	Tag
 };
