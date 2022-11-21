fad0479a8be2b2850a123f4c0123dd9ca26e73d8
Im0Low <maxipaltri@gmail.com>
2022-11-21 18:50:17 +0100

    progressbar example added

--- CloudKit/src/routes/+page.svelte
+++ CloudKit/src/routes/+page.svelte
@@ -7,10 +7,13 @@
 	import RadioButton from '$lib/components/user/radioButton.svelte';
 	import Checkbox from '$lib/components/user/checkbox.svelte';
 	import InputText from '$lib/components/user/input/inputText.svelte';
+	import ProgressBar from '$lib/components/layout/progressBar.svelte';
 
 	const contextMenuConfig: CloudKit.ContextMenu.config = {
 		items: [{ value: 'rename' }, { value: 'alert', func: () => alert(1) }, { value: 'close' }]
 	};
+
+	$: progress = 0;
 </script>
 
 <div id="index" class="transition-all-250">
@@ -26,6 +29,21 @@
 			<Checkbox checked label="okay lets go" tooltip="lool" size="lg" />
 			<Checkbox label="okay lets go" />
 			<InputText />
+
+			<div class="flex flex-vr gap-3">
+				<Button
+					func={() => {
+						let interval = setInterval(() => {
+							if (progress <= 99) progress++;
+							else clearInterval(interval);
+						}, 100);
+					}}>Start</Button
+				>
+				<ProgressBar percentage={progress} />
+				<ProgressBar theme="special" percentage={progress} />
+				<ProgressBar theme="light" percentage={progress} />
+				<ProgressBar theme="primary" percentage={progress} />
+			</div>
 		</div>
 	</div>
 </div>
