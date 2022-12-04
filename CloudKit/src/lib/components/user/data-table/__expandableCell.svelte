e170b82cb9ed0ae8464ec405ad1a32e96d2742f6
Im0Low <maxipaltri@gmail.com>
2022-12-04 15:17:53 +0100

    extra data cell changed

--- CloudKit/src/lib/components/user/data-table/__expandableCell.svelte
+++ CloudKit/src/lib/components/user/data-table/__expandableCell.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
-	// TYPES
-	import type { ExtraElements as elementType } from '$lib/ts/enum/DataTable';
 	// COMPONENTS
 	import Tag from '../../layout/Tag.svelte';
 	// EXPORTS
-	export let elementType: elementType;
-	export let elementData: { text: string; tagColor?: string }[];
+	export let title: string = '';
+	export let data: string;
+	export let tagColor = '';
 </script>
 
-{#if elementType && elementData}
-	<div class="expanded-cell gap-1 hide m-t-4">
-		<h5 class="cell-title">{elementType}</h5>
-		{#each elementData as data}
-			{#if !data.tagColor}
-				<h5>{data.text}</h5>
-			{:else}
-				<Tag size="xs" color={data.tagColor}>{data.text}</Tag>
-			{/if}
-		{/each}
-	</div>
-{/if}
+<div class="expanded-cell gap-1 hide m-t-4">
+	{#if title}
+		<h5 class="cell-title">{title}</h5>
+	{/if}
+	{#if !tagColor}
+		<h5>{data}</h5>
+	{:else}
+		<Tag size="xs" color={tagColor}>{data}</Tag>
+	{/if}
+</div>
 
 <style lang="scss">
 	.expanded-cell .cell-title {
