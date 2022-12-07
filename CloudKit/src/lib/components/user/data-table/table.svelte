7783af6e721709e1d38be8fc24d8dd21538f831e
Im0Low <maxipaltri@gmail.com>
2022-12-07 09:32:20 +0100

    improvments and contextmenu fix

--- CloudKit/src/lib/components/user/data-table/table.svelte
+++ CloudKit/src/lib/components/user/data-table/table.svelte
@@ -15,6 +15,7 @@ You add data using the dataTableStore with the unique id you receive by using th
 	import { dataTableStore, type TableInstance } from '$lib/ts/stores/dataTable';
 	// UTILITY
 	import { extractObjValues } from '$lib/ts/utility/array';
+	import { pause } from '$lib/ts/utility/async';
 	// COMPONENTS
 	import ExpandableCell from './__expandableCell.svelte';
 	import Dropdown from '$lib/components/user/dropdown.svelte';
@@ -52,6 +53,7 @@ console.info(tableId()); // 2m3y1
 	export let borders = false;
 	// LOGIC
 	let tableId: string = '';
+	let reference: Element;
 	let tableInstance: Readable<TableInstance> | undefined;
 	$: tableInstance;
 	$: mounted = false;
@@ -62,6 +64,8 @@ console.info(tableId()); // 2m3y1
 		{ type: 'sort', values: ['Sort'] }
 	];
 
+	let refreshListeners = () => {};
+
 	function collapseRow(event: MouseEvent) {
 		// @ts-ignore
 		toggleOnEvent((event.currentTarget as HTMLDivElement).parentNode.parentNode, 'collapsed');
@@ -71,27 +75,29 @@ console.info(tableId()); // 2m3y1
 		(event as HTMLDivElement).classList.toggle(_class);
 	}
 
-	onMount(() => (mounted = true));
 	beforeUpdate(() => {
-		if (!tableInstance && !tableId.length) {
+		if (!tableId.length) {
 			tableId = dataTableStore.registerTable(columns.length) ?? '';
 			tableInstance = dataTableStore.table(tableId ?? '');
+			tableInstance?.subscribe((_) => pause(100).then(() => refreshListeners()));
 		}
 	});
+	onMount(() => (mounted = true));
 	onDestroy(() => dataTableStore.destroyTable(tableId));
-
-	// TODO selector has parent reference
 </script>
 
 {#if contextMenuConfig && mounted}
 	<TableContextMenu
-		items={contextMenuConfig.items}
-		targetsSelector=".data-table .table-container .data-row"
+		bind:refreshListeners
 		commonFunction={contextMenuConfig.commonFunction}
 		contextMenuEvent={contextMenuConfig.contextMenuEvent}
+		targetsSelector=".data-table .table-container .data-row"
+		{reference}
+		items={contextMenuConfig.items}
 	/>
 {/if}
-<div class="data-table flex-col gap-3">
+
+<div class="data-table flex-col gap-3" bind:this={reference}>
 	<div class="table-tools">
 		<div class="caption-inner flex gap-2">
 			{#each sorting as sorter}
@@ -109,7 +115,7 @@ console.info(tableId()); // 2m3y1
 						{/if}
 						<th class="h4 bold">
 							{#if column.img}
-								<img alt="" class="icon" src={`/assets/${column.img}.svg`} />
+								<img alt="" class="icon" src={column.img} />
 							{/if}
 							{column.name}
 						</th>
@@ -118,51 +124,55 @@ console.info(tableId()); // 2m3y1
 						<th class="column-collapse p-0" />
 					{/if}
 				</tr>
-				{#each $tableInstance?.rowsData ?? [] as tableRow, rowIndex}
-					<tr class="data-row" class:row-context={contextMenuConfig}>
-						<td class="h4 text-center bold">{rowIndex + 1}</td>
-						{#each tableRow as tableCell}
-							<td class="vr-align-top">
-								<h4 class="flex w-100">
-									{#if tableCell.img}
-										<img alt="" class="icon" src={`/assets/${tableCell.img}.png`} />
-									{/if}
-									{tableCell.content}
-								</h4>
+				{#if $tableInstance?.rowsData?.length}
+					{#each $tableInstance.rowsData as tableRow, rowIndex}
+						{#if tableRow?.length}
+							<tr class="data-row" class:row-context={contextMenuConfig}>
+								<td class="h4 text-center bold">{rowIndex + 1}</td>
+								{#each tableRow as tableCell}
+									<td class="vr-align-top">
+										<h4 class="flex w-100">
+											{#if tableCell.img}
+												<img alt="" class="icon" src={tableCell.img} />
+											{/if}
+											{tableCell.content}
+										</h4>
+										{#if extraData}
+											<ExpandableCell
+												title={tableCell.extra?.title}
+												data={tableCell.extra?.data ?? ''}
+												tagColor={tableCell.extra?.tagColor}
+											/>
+										{/if}
+									</td>
+								{/each}
 								{#if extraData}
-									<ExpandableCell
-										title={tableCell.extra?.title}
-										data={tableCell.extra?.data ?? ''}
-										tagColor={tableCell.extra?.tagColor}
-									/>
+									<td class="row-collapse p-0">
+										<!-- svelte-ignore a11y-click-events-have-key-events -->
+										<div
+											class="collapse-btn flex flex-all rlv pointer"
+											on:click={collapseRow}
+											title="Expand view"
+										>
+											<svg
+												width="25"
+												height="25"
+												viewBox="0 0 25 25"
+												fill="none"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<path
+													d="M12.5 17.7083C12.2341 17.707 11.9718 17.6465 11.7321 17.5312C11.4925 17.4159 11.2816 17.2487 11.1146 17.0417L6.72916 11.7292C6.47291 11.4094 6.31165 11.024 6.26376 10.617C6.21588 10.2099 6.2833 9.79766 6.45833 9.42709C6.60029 9.10504 6.83195 8.83065 7.12564 8.6367C7.41933 8.44275 7.76265 8.33744 8.11458 8.33334H16.8854C17.2373 8.33744 17.5807 8.44275 17.8744 8.6367C18.168 8.83065 18.3997 9.10504 18.5417 9.42709C18.7167 9.79766 18.7841 10.2099 18.7362 10.617C18.6883 11.024 18.5271 11.4094 18.2708 11.7292L13.8854 17.0417C13.7184 17.2487 13.5075 17.4159 13.2678 17.5312C13.0282 17.6465 12.7659 17.707 12.5 17.7083Z"
+													fill="#ffffff91"
+												/>
+											</svg>
+										</div>
+									</td>
 								{/if}
-							</td>
-						{/each}
-						{#if extraData}
-							<td class="row-collapse p-0">
-								<!-- svelte-ignore a11y-click-events-have-key-events -->
-								<div
-									class="collapse-btn flex flex-all rlv pointer"
-									on:click={collapseRow}
-									title="Expand view"
-								>
-									<svg
-										width="25"
-										height="25"
-										viewBox="0 0 25 25"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											d="M12.5 17.7083C12.2341 17.707 11.9718 17.6465 11.7321 17.5312C11.4925 17.4159 11.2816 17.2487 11.1146 17.0417L6.72916 11.7292C6.47291 11.4094 6.31165 11.024 6.26376 10.617C6.21588 10.2099 6.2833 9.79766 6.45833 9.42709C6.60029 9.10504 6.83195 8.83065 7.12564 8.6367C7.41933 8.44275 7.76265 8.33744 8.11458 8.33334H16.8854C17.2373 8.33744 17.5807 8.44275 17.8744 8.6367C18.168 8.83065 18.3997 9.10504 18.5417 9.42709C18.7167 9.79766 18.7841 10.2099 18.7362 10.617C18.6883 11.024 18.5271 11.4094 18.2708 11.7292L13.8854 17.0417C13.7184 17.2487 13.5075 17.4159 13.2678 17.5312C13.0282 17.6465 12.7659 17.707 12.5 17.7083Z"
-											fill="#ffffff91"
-										/>
-									</svg>
-								</div>
-							</td>
+							</tr>
 						{/if}
-					</tr>
-				{/each}
+					{/each}
+				{/if}
 			</tbody>
 		</table>
 	</div>
