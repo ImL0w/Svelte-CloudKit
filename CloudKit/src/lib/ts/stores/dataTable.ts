54dbc4754ef5b39b31979a6b29a01031a80c0c13
Im0Low <maxipaltri@gmail.com>
2022-12-07 09:33:14 +0100

    major refactor and fixes + remove row

--- CloudKit/src/lib/ts/stores/dataTable.ts
+++ CloudKit/src/lib/ts/stores/dataTable.ts
@@ -46,84 +46,104 @@ class DataTableStore {
 	 */
 	table(id: string) {
 		if (id.length) {
-			const arrayPosition = this.__tableIdToArrayPos(id);
-			if (arrayPosition !== -1)
-				return derived(this.__tables, ($tables) => {
-					return $tables.instances[arrayPosition];
-				});
+			const validId = this.__validId(id);
+			if (validId.valid) return derived(this.__tables, (tables) => tables.instances[validId.index]);
 		}
 	}
 
 	/**
 	 * Method responsable for registering your table (register only once! onMount)
-	 * @param maxColumns Columns the table has. Note: Must be correct otherwise you could accidentally add more then the table has of columns
+	 *
+	 * This method is only used inside the `table` component
+	 * @param maxColumns >= 1 Columns the table has. Note: Must be correct otherwise you could accidentally add more then the table has of columns
 	 * @returns Newly created id, or null if something went wrong.
 	 */
 	registerTable(maxColumns: number): string | null {
 		if (maxColumns >= 1) {
 			const randId = randomString(5);
-			if (this.__tableIdToArrayPos(randId) === -1) {
+			if (!this.__validId(randId).valid) {
 				this.__tables.update((tables) => {
-					tables.instances.push(this.__newInstance(randId, maxColumns));
-					return tables;
+					return { instances: [...tables.instances, this.__newInstance(randId, maxColumns)] };
 				});
 				return randId;
 			}
 		}
 		return null;
 	}
-
+	/**
+	 * Removes a table instance from the writable store
+	 *
+	 * This method is only used inside the `table` component.
+	 * @param id
+	 */
 	destroyTable(id: string) {
 		if (id.length) {
-			if (this.__tableIdToArrayPos(id) !== -1)
+			if (this.__validId(id).valid)
 				this.__tables.update((tables) => {
 					return { instances: tables.instances.filter((table) => table.id !== id) };
 				});
 		}
 	}
-
 	/**
 	 * This method adds data to a specifc table
 	 * @param id Id of the table
 	 * @param rowData Data to be added - Note: If the data is invalid, the function will return without warnings!
 	 */
 	addData(id: string, rowData: TableRowData) {
-		const arrayPosition = this.__tableIdToArrayPos(id);
-		if (arrayPosition !== -1)
+		const validId = this.__validId(id);
+		if (validId.valid)
 			this.__tables.update((tables) => {
-				const maxColumns = tables.instances[arrayPosition].maxColumns;
-				if (rowData.length === maxColumns) {
-					const newRow = [];
-					newRow.push(...rowData);
-					tables.instances[arrayPosition].rowsData.push(newRow);
+				if (rowData.length === tables.instances[validId.index].maxColumns) {
+					tables.instances[validId.index].rowsData.push([...rowData]);
 				}
-
-				return {
-					instances: [...tables.instances]
-				};
+				return tables;
 			});
 	}
-
+	/**
+	 * Removes a row from a specific table
+	 * @param id Id of the table
+	 * @param rowIndex Row index >= 1
+	 */
+	removeRow(id: string, rowIndex: number) {
+		const validId = this.__validId(id);
+		if (validId.valid && rowIndex >= 1) {
+			this.__tables.update((tables) => {
+				tables.instances[validId.index].rowsData = tables.instances[validId.index].rowsData.filter(
+					(_, _rowIndex) => rowIndex - 1 !== _rowIndex
+				);
+				return tables;
+			});
+		}
+	}
+	/**
+	 * Creates a new table instance object
+	 * @param id
+	 * @param maxColumns
+	 * @returns New valid instance
+	 */
 	private __newInstance(id: string, maxColumns: number): TableInstance {
 		return { id: id, rowsData: [], maxColumns: maxColumns };
 	}
-
-	private __tableIdToArrayPos(id: string) {
-		let index = -1;
-		this.__tablesSubscribed?.instances.forEach((table, _index) => {
-			if (table.id === id) index = _index;
+	/**
+	 * Validates a given id and returns index array position
+	 * @param id
+	 * @returns Object { valid: boolean; index: number | -1 }
+	 */
+	private __validId(id: string): { valid: boolean; index: number | -1 } {
+		let array_position = -1;
+		this.__tablesSubscribed?.instances.forEach((table, index) => {
+			if (table.id === id) array_position = index;
 		});
-		return index;
+		return { valid: array_position !== -1, index: array_position };
 	}
 }
 
 /**
  * Store responsable to handle all tables and their data
- * 
+ *
  * Warning: `registerTable` & `destroyTable` shouldn't be used because only the component `Table` uses them. You don't have to!
  * @example import { dataTableStore } from '@cloudscript.live/svelte-cloudkit';
-
-dataTableStore.addData(tableId, newRowData);
+ * dataTableStore.addData(tableId, newRowData);
  */
 const dataTableStore = new DataTableStore();
 
